b0f48274d3ce111ff03669339e3f103cdb7657f2
Suhas B Umesh <60173890+suhasbu@users.noreply.github.com>
2020-04-28 16:57:05 -0700

    Update pickle.py

--- pickle.py
+++ pickle.py
@@ -1,13 +1,15 @@
 import pickle
+from sklearn.externals import joblib 
+
 # filename = 'classifier.ipynb'
 # pickle.dump(model_k, open(filename, 'wb'))
  
 # some time later...
  
 # load the model from disk
-def final_classify():
+def final_classify(X_train):
     
-    loaded_model = pickle.load(open('./data/model.sav', 'rb'))
+    loaded_model = joblib.load(open('./data/model.pkl'))
     result = loaded_model.predict(X_train)
     return result
 
